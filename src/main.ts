4a343ef940267709520a3fcb1e72a48cf29aa586
White Pai <white_pai@appdet.com>
2022-02-24 23:16:02 +0800

    feat: import axios

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import { router } from "./router.js";
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
 import "virtual:windi.css";
 import "virtual:windi-devtools";
@@ -8,6 +10,7 @@ import "virtual:svg-icons-register";
 
 const app = createApp(App);
 
-app.use(router);
+app.use(router).use(VueAxios, axios);
+app.provide('axios', app.config.globalProperties.axios);
 
 app.mount("#app");
